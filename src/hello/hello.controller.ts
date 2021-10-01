d6a309d977d655dacaf87fa1d5d3a36eb5ec1541
nico-php-coder <69898039+nico-php-coder@users.noreply.github.com>
2021-10-01 16:32:18 +0200

    initial commit and part 1 complete

---
+++ src/hello/hello.controller.ts
@@ -0,0 +1,51 @@
+import {Controller, Logger, Get, NotFoundException, Param, Post, Body} from "@nestjs/common";
+import {HelloBodyDTO} from "./hello-body.dto";
+import {v4 as uuid} from "uuid";
+import { HelloService } from "./hello.service";
+
+@Controller()
+export class HelloController {
+
+  // a logger from nestjs for loggin error/info
+  logger: Logger = new Logger(HelloController.name);
+
+  constructor(private readonly helloService : HelloService) {}
+
+  @Get('hello')
+  async replyHello() {
+    try {
+      return 'hello';
+    } catch(error) {
+      this.logger.error(error?.message ?? '');
+      throw error;
+    }
+  }
+
+  @Get('hello/:helloId') // dynamic parameter
+  async replyExactHello(@Param('helloId') id: string) {
+    try {
+      let hello = await this.helloService.findById(id);
+      let message = hello.message;
+      if(!message) {
+        throw new NotFoundException('desired `hello` not found'); // 404
+      }
+      return message;
+    } catch(error) {
+      this.logger.error(error?.massage ?? '');
+      throw error;
+    }
+  }
+
+  @Post('hello')
+  saveHello(@Body() body: HelloBodyDTO) {
+    console.log(body);
+    try {
+      const id = uuid();
+      const hello = {id, message: body.message};
+      this.helloService.create(hello.message);
+    } catch(error) {
+      this.logger.error(error?.message ?? '');
+      throw error;
+    }
+  }
+}
---
+++ src/hello/hello.service.ts
@@ -0,0 +1,33 @@
+import { Injectable } from "@nestjs/common";
+import { v4 as uuid } from "uuid";
+
+@Injectable()
+export class HelloService {
+  // db: {id: string, message: string}[] = [];
+  db: {id: string, message: string}[] = [
+    {id: "1", message: "hello"},
+    {id: "2", message: "this"},
+    {id: "3", message: "is"},
+    {id: "4", message: "a"},
+    {id: "5", message: "test"},
+    {id: "6", message: "lol"},
+  ];
+
+  async findById(id: string) {
+    return this.db.find(element => element.id == id);
+  }
+
+  async create(message: string) {
+    const id = uuid();
+    const element = {id, message};
+    this.db.push(element);
+    return element;
+  }
+
+  async deleteById(id: string) {
+    this.db.filter(function(element) {
+      return element.id != id;
+    });
+    return `Delete node ${id} from db`;
+  }
+}
